1f8a6c8998b82e8addf41779654444067ea35944
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-09-15 17:13:07 -0400

    Fixed Bug with Insertion Sort coloring

--- client/insertionSort.js
+++ client/insertionSort.js
@@ -14,11 +14,13 @@ const insertionSort = async function (array, speed) {
     while (array[j] < array[j - 1] && j > 0) {
       await updateSingleBarColor(arrayBars, j, 'active-red', speed);
       [array[j], array[j - 1]] = [array[j - 1], array[j]];
-      await updateComparedBars(arrayBars, j, j - 1, 'active-purple', speed);
+      updateComparedBars(arrayBars, j, j - 1, 'active-purple', speed);
       j--;
       this.setState({ array });
     }
+    await updateSingleBarColor(arrayBars, j, 'active-green', speed);
     sfx.sorted.play();
+    await updateSingleBarColor(arrayBars, j, 'active-purple', speed);
   }
 
   end(array, arrayBars);
