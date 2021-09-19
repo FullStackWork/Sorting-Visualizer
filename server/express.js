ece973abf7055af92dcc29221584e76879b308a6
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-09-18 23:07:10 -0400

    server.js change

--- server/express.js
+++ server/express.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 
 app.get('/', function (req, res) {
-  res.sendFile('/Users/rohan/sorting-visualizer/public/index.html');
+  // res.sendFile('/Users/rohan/sorting-visualizer/public/index.html');
+  res.sendFile('../public/index.html');
 });
 app.use(express.static(path.join('/Users/rohan/sorting-visualizer/public')));
 
