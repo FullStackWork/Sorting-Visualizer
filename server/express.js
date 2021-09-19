9c309be1351926bd391edfc7a060142e57c62192
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-09-18 22:31:24 -0400

    server js change

--- server/express.js
+++ server/express.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.get('/', (req, res) =>
+  res.sendFile('/Users/rohan/sorting-visualizer/public/index.html')
+);
 app.use(express.static(path.join('/Users/rohan/sorting-visualizer/public')));
 
-app.get('/', function (req, res) {
-  res.send(path.join('/Users/rohan/sorting-visualizer/public'));
-});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
